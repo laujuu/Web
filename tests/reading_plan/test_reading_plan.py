a4f478dde2ad11b20387652bac33ee437a507d47
laujuu <lauroindcontato@gmail.com>
2023-03-15 23:21:40 -0300

    feat: test_reading_plan

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,40 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from unittest.mock import patch
+from tech_news.analyzer.reading_plan import ReadingPlanService
 
 
 def test_reading_plan_group_news():
-    pass
+    with patch("tech_news.analyzer.reading_plan.find_news") as mock_find_news:
+        mock_find_news.return_value = [
+            {
+                "title": "Notícia bacana",
+                "writer": "Eu",
+                "summary": "Algo muito bacana aconteceu",
+                "reading_time": 4,
+                "timestamp": "04/04/2021",
+                "category": "Ferramentas",
+            },
+            {
+                "title": "Notícia bacana 2",
+                "writer": "Você",
+                "summary": "Algo muito bacana aconteceu de novo",
+                "reading_time": 1,
+                "timestamp": "07/04/2022",
+                "category": "Novidades",
+            },
+            {
+                "title": "Notícia demorada",
+                "writer": "Eu",
+                "summary": "Um negócio rolou aí",
+                "reading_time": 30,
+                "timestamp": "04/04/2021",
+                "category": "Ferramentas",
+            },
+        ]
+        reading = ReadingPlanService().group_news_for_available_time(10)
+
+        assert len(reading["readable"]) == 2
+        assert len(reading["unreadable"]) == 1
+        assert reading["readable"][0]["unfilled_time"] == 6
+        assert reading["readable"][1]["unfilled_time"] == 10
+        with ValueError:
+            ReadingPlanService().group_news_for_available_time(0)
