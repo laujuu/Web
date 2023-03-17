23cafa361188e09ff4415fc74aa5a6e2d273d73f
laujuu <lauroindcontato@gmail.com>
2023-03-17 18:26:48 -0300

    feat: top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    db = get_collection()
+    pipeline = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5}
+    ]
+    result = list(db.aggregate(pipeline))
+    if not result:
+        return []
+    return [r["_id"] for r in result]
